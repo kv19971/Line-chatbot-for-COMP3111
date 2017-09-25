120f03721ae0efd8b5bd79d5e3b8a73d64abb0bc
kv19971 <kshitijverma999@hotmail.com>
2017-09-25 16:34:40 +0800

    lab3 sql added

--- sample-spring-boot-kitchensink/src/main/java/com/example/bot/spring/SQLDatabaseEngine.java
+++ sample-spring-boot-kitchensink/src/main/java/com/example/bot/spring/SQLDatabaseEngine.java
@@ -12,10 +12,26 @@ public class SQLDatabaseEngine extends DatabaseEngine {
 	@Override
 	String search(String text) throws Exception {
 		//Write your code here
-		if(text.equals("abc")) {
-			return new String("def");
+		try {
+			Connection con = this.getConnection();
+			PreparedStatement stmt = con.prepareStatement("SELECT response FROM lab3_table WHERE concat('', ?) LIKE concat('%', keyword, '%')");
+			stmt.setString(1, text);
+			ResultSet rs = stmt.executeQuery();
+			String rtval = null;
+			while(rs.next() && rtval == null) {
+				rtval = rs.getString(1);
+			}
+
+			rs.close();
+			stmt.close();
+			con.close();
+			log.info("RETURN VAL {}", rtval);
+			return rtval;
+		}catch(Exception e){
+			log.info("EXCEPTION {}", e);
 		}
-		return null;
+		return "";
+
 	}
 	
 	
